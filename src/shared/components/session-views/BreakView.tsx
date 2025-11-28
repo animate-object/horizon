d976fc7b3c4922e47e0b18a4b07d281eee5ac919
Alex Prinsen <alexander.f.prinsen@gmail.com>
2025-11-28 17:01:23 -0600

    touch up break page

--- src/shared/components/session-views/BreakView.tsx
+++ src/shared/components/session-views/BreakView.tsx
@@ -13,15 +13,23 @@ export function BreakView() {
 
       <Text.Body light>{t("break.thisIsAGoodTime")}</Text.Body>
       <Text.Body light>
-        <ul className="list">
-          <li>- {t("break.thisIsAGoodTime1")}</li>
-          <li>- {t("break.thisIsAGoodTime2")}</li>
-          <li>- {t("break.thisIsAGoodTime3")}</li>
+        <ul className="list list-disc ps-4">
+          <li>{t("break.thisIsAGoodTime1")}</li>
+          <li>{t("break.thisIsAGoodTime2")}</li>
+          <li>{t("break.thisIsAGoodTime3")}</li>
         </ul>
       </Text.Body>
-      <Text.Body>{t("break.why")}</Text.Body>
-      <Text.Body light>{t("break.whyExplanation1")}</Text.Body>
-      <Text.Body light>{t("break.whyExplanation2")}</Text.Body>
+      <div className="collapse bg-base-100 border border-base-300">
+        <input className="ps-0" type="radio" name="break-collapse" />
+        <div className="collapse-title font-semibold ps-0 py-1">
+          <Text.Body>{t("break.why")}</Text.Body>
+        </div>
+        <div className="collapse-content text-sm ps-2">
+          <Text.Body light>{t("break.whyExplanation1")}</Text.Body>
+          <br />
+          <Text.Body light>{t("break.whyExplanation2")}</Text.Body>
+        </div>
+      </div>
       <Text.SubHeader>
         {t("common.timeRemaining")}&nbsp;
         <CountdownClock timeRemainingSeconds={standard?.timeRemainingSeconds} />
